cb60e9af1c1cc23d424cceb03dae02ea93ee5d1f
weikai <weikai@fucotech.com.tw>
2022-05-10 15:06:58 +0800

    Add, send_mail with swagger.

--- django_celery/urls.py
+++ django_celery/urls.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect
 from django.urls import path, include, re_path
-from django_celery import tasks, views
+from django_celery import views
 
 urlpatterns = [
-    path('mail/',views.send_email, name='mail')
+    path('mail/',views.send_email, name='mail'),
+    path('sendmail/',views.SendEmailViewSet.as_view(),name='sendmail')
 ]
