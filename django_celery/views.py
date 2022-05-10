dd62ea2d28f5f27695adae2d4c8ed051807fe9b2
weikai <weikai@fucotech.com.tw>
2022-05-10 15:06:58 +0800

    Add, send_mail with swagger.

--- django_celery/views.py
+++ django_celery/views.py
@@ -1,7 +1,12 @@
 import re
 from django.http import JsonResponse
 from django_celery import tasks 
+from drf_yasg.utils import swagger_auto_schema
 from celery.result import AsyncResult
+from rest_framework import  generics
+from rest_framework.response import Response
+from rest_framework import permissions
+from drf_yasg import openapi
 
 # Create your views here.
 
@@ -16,3 +21,27 @@ def send_email(request,*args,**kwargs):
         return JsonResponse({'status':result.status,'task_id':result.task_id})
     else:
         return JsonResponse({'status':None,'task_id':None})
+
+class SendEmailViewSet(generics.GenericAPIView):
+    permission_classes = [permissions.IsAdminUser]
+
+    @swagger_auto_schema(
+        operation_summary='寄送Email',
+        operation_description='使用非同步進行寄送Email',
+        manual_parameters=[
+            openapi.Parameter('recipient_email', openapi.IN_QUERY, description="收件者email", type=openapi.TYPE_STRING),
+            openapi.Parameter('recipient_name', openapi.IN_QUERY, description="收件者名稱", type=openapi.TYPE_STRING)
+        ]
+    )
+
+    def get(self, request, format=None):
+        recipient_email = request.GET.get('recipient_email',None)
+        recipient_name = request.GET.get('recipient_name',None)
+        if recipient_email != None and recipient_name != None:
+            print(recipient_email,recipient_name)
+            res=tasks.send_email.delay(recipient_email,recipient_name)
+            result = AsyncResult(res.task_id)
+            print('send_email',result)
+            return Response({'status':result.status,'task_id':result.task_id})
+        else:
+            return Response({'status':None,'task_id':None})
