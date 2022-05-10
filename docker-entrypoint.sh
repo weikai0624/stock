30a3d1346a041c661e8a5019e1d11b9c042a6f40
weikai0624 <53070059+weikai0624@users.noreply.github.com>
2022-05-10 14:19:51 +0800

    Add, celery docker (#8)
    * Feature, celery
    
    * Feature, celery docker
    
    Co-authored-by: weikai <weikai@fucotech.com.tw>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,15 @@
 # docker-entrypoint.sh
 #!/bin/bash
-
 # Collect static files
 # echo "Collect static files"
 # python manage.py collectstatic --noinput
-
 # Make migrations
 echo "Make migrations"
 python manage.py makemigrations
-
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
-
 #Create Seeds
 echo "Create Seeds"
 python tools/seeds/make_data.py
-
-python manage.py runserver 8008
-
 exec "$@"
